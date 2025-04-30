bed32149fbdb5e914d3eb0c08a08df0d2b6d20b5
mxolisi <mxolisi.gojolo@gmail.com>
2025-04-30 09:09:44 +0200

    final project

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => populateSummary(data))
+      .catch(error => console.error('Error loading data:', error));
+  });
+  
+  function populateSummary(data) {
+    const summaryList = document.querySelector('.summary-list');
+    summaryList.innerHTML = ''; 
+  
+    data.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('summary-item', item.category.toLowerCase());
+  
+      listItem.innerHTML = `
+        <div class="icon-label">
+          <img src="${item.icon}" alt="${item.category} icon" />
+          <span>${item.category}</span>
+        </div>
+        <span class="score-text">${item.score} <span class="out-of">/ 100</span></span>
+      `;
+  
+      summaryList.appendChild(listItem);
+    });
+  }
+  
